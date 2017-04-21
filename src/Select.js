a18a8b942f4e7a2b691318dcb85b4e662cf43a86
Will Deuschle <wjdeuschle@gmail.com>
2017-04-21 18:15:04 -0400

    fix for loading on open

--- src/Select.js
+++ src/Select.js
@@ -203,9 +203,15 @@ const Select = React.createClass({
 	componentWillUpdate (nextProps, nextState) {
 		if (nextState.isOpen !== this.state.isOpen) {
 			this.toggleTouchOutsideEvent(nextState.isOpen);
-            console.log("HELLO TESTING")
 			const handler = nextState.isOpen ? nextProps.onOpen : nextProps.onClose;
 			handler && handler();
+            // this is ya boy will coming at you with a fix to the react-select
+            // source code. if we have a prop indicating that we only want to load
+            // on open, and we are opening, then load our data
+            if (this.props.loadOnOpen && nextState.isOpen && this.props.loadOptions) {
+                // load data
+                this.props.loadOptions('')
+            }
 		}
 	},
 
